d7278aa669b04e1187e642dd5047be2f81ec662b
SONALSINGH45 <99395129+SONALSINGH45@users.noreply.github.com>
2022-06-14 17:23:38 +0530

    Add files via upload
    timedisplay-project on react.js

---
+++ App.jsx
@@ -0,0 +1,29 @@
+import React ,{useState} from "react";
+
+const App =() =>{
+
+
+    let newTime= new Date().toLocaleTimeString();
+
+    const [ctime, setCtime]=useState(newTime);
+
+
+    const UpdateTime=()=>{
+     let    newCTime= new Date().toLocaleTimeString();
+        setCtime(newCTime);
+
+    }
+    return(
+        <>
+        <h1>{ctime}</h1> 
+        <button onClick={UpdateTime}>  get Time</button>
+    
+        
+        
+        
+        </>
+
+    );
+}
+
+export default App
